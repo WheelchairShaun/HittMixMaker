15f4ccf77a494d8b4c1378bc8c70e51d658168ef
Shaun Barnes <shaun.w.barnes@gmail.com>
2020-02-06 16:23:30 -0500

    Added playlist class #1

---
+++ WheelchairShaun.HittMixMaker.Core/ISong.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WheelchairShaun.HittMixMaker.Core
+{
+	public interface ISong
+	{
+		public string FilePath { get; set; }
+		public string Song { get; set; }
+		public int Length { get; set; }
+		public string Artist { get; set; }
+		public string Album { get; set; }
+		public string AlbumArtist { get; set; }
+		public string Composer { get; set; }
+		public string Grouping { get; set; }
+		public string Genre { get; set; }
+		public int Year { get; set; }
+		public int Track { get; set; }
+		public int AlbumTracks { get; set; }
+		public int Disc { get; set; }
+		public int AlbumDiscs { get; set; }
+		public bool Compilation { get; set; }
+	}
+}
---
+++ WheelchairShaun.HittMixMaker.Core/Playlist.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WheelchairShaun.HittMixMaker.Core
+{
+	public class Playlist
+	{
+		private const string FILE_EXTENSION = "m3u";
+		private const string FILE_HEADER = "#EXTM3U";
+		private const string EXTENDED_INFO = "#EXTINF:";
+
+		private IList<ISong> _songs;
+
+		public Playlist()
+		{
+			_songs = new List<ISong>();
+		}
+
+		public Playlist(IList<ISong> songs)
+		{
+			_songs = songs;
+		}
+
+		public Playlist(string filePath, string name)
+		{
+			_songs = new List<ISong>();
+			FilePath = filePath;
+			Name = name;
+		}
+
+		public Playlist(IList<ISong> songs, string filePath, string name)
+		{
+			_songs = songs;
+			FilePath = filePath;
+			Name = name;
+		}
+
+		public string FilePath { get; set; }
+		public string FileExtension { get { return FILE_EXTENSION; } }
+		public string FileHeader { get { return FILE_HEADER; } }
+		public string ExtendedInfo { get { return EXTENDED_INFO; } }
+		public string Name { get; set; }
+
+		public IList<ISong> Songs
+		{
+			get { return _songs; }
+		}
+
+
+
+	}
+}
